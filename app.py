bd86b045395c731e29f9f5d017127297a5b5b60b
Rahulmane9403 <Rahulmane9403@gmail.com>
2022-04-03 02:51:47 +0530

    push

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def transform_text(text):
 #tfidf = pickle.load(open('vectorizer.pkl','rb'))
 #model = pickle.load(open('model.pkl','rb'))
 
-st.title("SMS Spam Dectection")
+st.title("Sms Spam Dectection")
 
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
